a250e33027734793363d5cac54768d44e5ad184c
behill90 <44953745+behill90@users.noreply.github.com>
2021-12-01 13:07:38 -0500

    Update Day1.py

--- Day1.py
+++ Day1.py
@@ -13,3 +13,11 @@ for i in range(len(sub)-1):
 print(count)
 
 # part 2 #
+count1 = 0
+for i in range(len(sub)-3):
+    first = int(sub[i]) + int(sub[i+1]) + int(sub[i+2])
+    second = int(sub[i+1]) + int(sub[i+2]) + int(sub[i+3])
+    if second > first:
+        count1 += 1
+
+print(count1)
